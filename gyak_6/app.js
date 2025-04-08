d4320d01add3f87774e77f52b594e367c0859aaa
dvd2001 <bedavid2001@gmail.com>
2025-04-08 11:35:53 +0200

    gyak8

--- gyak_6/app.js
+++ gyak_6/app.js
@@ -1,13 +1,31 @@
-const GITHUB_ACCESS_TOKEN='';
+const GITHUB_ACCESS_TOKEN = '';
 
 function loadNavigation() {
     fetch('nav.html')
         .then(res => res.text())
         .then(navHtml => {
-            const body=document.querySelector('body');
+            const body = document.querySelector('body');
             body.insertAdjacentHTML('afterbegin', navHtml);
         })
         .catch(err => console.error(err));
 }
 
+async function loadUserData() {
+    const res = await fetch('https://api.github.com/user', {
+        headers: {
+            Authorization: `token ${GITHUB_ACCESS_TOKEN}`
+        }
+    });
+
+    //TODO: hibakezelés
+
+    const user = await res.json();
+    sessionStorage.setItem('username', user.login);
+}
+
 loadNavigation();
+
+const username = sessionStorage.getItem('username');
+if (!username) {
+    loadUserData();
+}
